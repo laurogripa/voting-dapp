94fd6a38367bac26302ceaab50f07009c8f892e5
Lauro Gripa Neto <lauro@magrathealabs.com>
2018-03-01 22:56:05 -0300

    Redesign

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,89 +1,31 @@
 pragma solidity ^0.4.18;
+// We have to specify what version of compiler this code will compile with
 
 contract Voting {
-  struct voter {
-    address addr;
-    uint tokens;
-    uint[] tokensUsedPerCandidate;
-  }
-
-  mapping (address => voter) public voterInfo;
-  mapping (bytes32 => uint) public votesReceived;
-
+  mapping (bytes32 => uint8) public votesReceived;
+  
   bytes32[] public candidateList;
 
-  uint public totalTokens;
-  uint public balanceTokens;
-  uint public tokenPrice;
-
-  function Voting(uint tokens, uint pricePerToken, bytes32[] candidateNames) public {
+  function Voting(bytes32[] candidateNames) public {
     candidateList = candidateNames;
-    totalTokens = tokens;
-    balanceTokens = tokens;
-    tokenPrice = pricePerToken;
   }
 
-  function totalVotesFor(bytes32 candidate) view public returns (uint) {
+  function totalVotesFor(bytes32 candidate) view public returns (uint8) {
+    require(validCandidate(candidate));
     return votesReceived[candidate];
   }
 
-  function voteForCandidate(bytes32 candidate, uint votesInTokens) public {
-    uint index = indexOfCandidate(candidate);
-    require(index != uint(-1));
-
-    if (voterInfo[msg.sender].tokensUsedPerCandidate.length == 0) {
-      for(uint i = 0; i < candidateList.length; i++) {
-        voterInfo[msg.sender].tokensUsedPerCandidate.push(0);
-      }
-    }
-
-    uint availableTokens = voterInfo[msg.sender].tokens - totalTokensUsed(voterInfo[msg.sender].tokensUsedPerCandidate);
-    require(availableTokens >= votesInTokens);
-
-    votesReceived[candidate] += votesInTokens;
-
-    voterInfo[msg.sender].tokensUsedPerCandidate[index] += votesInTokens;
+  function voteForCandidate(bytes32 candidate) public {
+    require(validCandidate(candidate));
+    votesReceived[candidate] += 1;
   }
 
-  function totalTokensUsed(uint[] _tokensUsedPerCandidate) private pure returns (uint) {
-    uint totalUsedTokens = 0;
-    for(uint i = 0; i < _tokensUsedPerCandidate.length; i++) {
-      totalUsedTokens += _tokensUsedPerCandidate[i];
-    }
-    return totalUsedTokens;
-  }
-
-  function indexOfCandidate(bytes32 candidate) view public returns (uint) {
+  function validCandidate(bytes32 candidate) view public returns (bool) {
     for(uint i = 0; i < candidateList.length; i++) {
       if (candidateList[i] == candidate) {
-        return i;
+        return true;
       }
     }
-    return uint(-1);
-  }
-
-  function buy() payable public returns (uint) {
-    uint tokensToBuy = msg.value / tokenPrice;
-    require(tokensToBuy <= balanceTokens);
-    voterInfo[msg.sender].addr = msg.sender;
-    voterInfo[msg.sender].tokens += tokensToBuy;
-    balanceTokens -= tokensToBuy;
-    return tokensToBuy;
-  }
-
-  function tokensSold() view public returns (uint) {
-    return totalTokens - balanceTokens;
-  }
-
-  function voterDetails(address user) view public returns (uint, uint[]) {
-    return (voterInfo[user].tokens, voterInfo[user].tokensUsedPerCandidate);
-  }
-
-  function transferTo(address account) public {
-    account.transfer(this.balance);
-  }
-
-  function allCandidates() view public returns (bytes32[]) {
-    return candidateList;
+    return false;
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Voting, 100, web3.toWei(0.000001, 'ether'), ['Hendrix', 'Slash', 'Chimbinha']);
+  deployer.deploy(Voting, ['Bolacha', 'Biscoito'], { gas: 6700000 });
 };
